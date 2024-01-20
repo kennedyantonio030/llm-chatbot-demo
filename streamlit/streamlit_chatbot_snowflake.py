39b70c2a2d4defadcb86516f626de8d73387ee35
kennedyantonio030 <kennedyantonio030@gmail.com>
2024-01-20 12:31:46 -0600

    Kind of working progress

--- streamlit/streamlit_chatbot_snowflake.py
+++ streamlit/streamlit_chatbot_snowflake.py
@@ -4,37 +4,38 @@ from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders.dataframe import DataFrameLoader
 from langchain.vectorstores import FAISS
-import tempfile
-
 
 OPEN_AI_KEY = os.environ.get("OPEN_AI_KEY")
+st.session_state.open_ai_key = OPEN_AI_KEY
 
-uploaded_file = st.sidebar.file_uploader("upload", type="csv")
 
-if uploaded_file:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
+conn = st.connection("snowflake")
+snowflake_table = conn.session()
+df = snowflake_table.table("nndb").to_pandas()
+df = df.dropna(subset=["WIKI_DEATH_SUMMARY"]).head(100)
 
-    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
+if len(df) is not None:
+    loader = DataFrameLoader(df, "WIKI_DEATH_SUMMARY")
     data = loader.load()
-    
-    st.write(data)
 
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.open_ai_key)
     vectors = FAISS.from_documents(data, embeddings)
 
     chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(
-            temperature=0.0, model_name="gpt-3.5-turbo", openai_api_key=OPEN_AI_KEY
+            temperature=0.0,
+            model_name="gpt-3.5-turbo",
+            openai_api_key=st.session_state.open_ai_key,
         ),
         retriever=vectors.as_retriever(),
     )
 
     def conversational_chat(query):
-        result = chain({"question": query, "chat_history": st.session_state["history"]})
+        result = chain({"question": query,
+                        "chat_history": st.session_state["history"]}
+                       )
         st.session_state["history"].append((query, result["answer"]))
 
         return result["answer"]
@@ -44,7 +45,7 @@ if uploaded_file:
 
     if "generated" not in st.session_state:
         st.session_state["generated"] = [
-            "Hello ! Ask me anything about " + uploaded_file.name + " 🤗 "
+            "Hello ! Ask me anything about your DataFrame" + " 🤗 "
         ]
 
     if "past" not in st.session_state:
@@ -75,8 +76,8 @@ if uploaded_file:
                     st.session_state["past"][i],
                     is_user=True,
                     key=str(i) + "_user",
-                    avatar_style="big-smile",
+                    avatar_style="adventurer",
                 )
                 message(
-                    st.session_state["generated"][i], key=str(i), avatar_style="thumbs"
+                    st.session_state["generated"][i], key=str(i), avatar_style="bottts"
                 )
