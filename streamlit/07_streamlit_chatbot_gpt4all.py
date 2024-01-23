892e3a73a80bdd5dba2c804df4751030618d9dd1
kennedyantonio030 <kennedyantonio030@gmail.com>
2024-01-23 09:09:55 -0600

    Fixed up memory and prompt templates

--- streamlit/07_streamlit_chatbot_gpt4all.py
+++ streamlit/07_streamlit_chatbot_gpt4all.py
@@ -1,35 +1,41 @@
 import streamlit as st
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 from langchain_community.llms import GPT4All
-from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.prompt import PromptTemplate
 
 
 @st.cache_resource
 def create_llm_instance():
     # Set up the prompt template
-    template = """Question: {question}
+    template = """
+    The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-    Answer: """
-    prompt = PromptTemplate(template=template, input_variables=["question"])
+    Current conversation:
+    {history}
+    Human: {input}
+    AI Assistant:
+    """  # noqa: E501
+
+    prompt = PromptTemplate(input_variables=["history", "input"], template=template)  # noqa: E501
 
     # Set up the local instace of the LLM
     local_path = "/Users/brianroepke/Library/Application Support/nomic.ai/GPT4All/mistral-7b-openorca.Q4_0.gguf"  # noqa: E501
-    callbacks = [StreamingStdOutCallbackHandler()]
-    memory = ConversationBufferMemory()
-    llm = GPT4All(model=local_path, callbacks=callbacks, verbose=True, streaming=True)  # noqa: E501
-    # llm_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
+    llm = GPT4All(model=local_path, verbose=True)
 
-    llm_chain = ConversationChain(llm=llm)
+    conversation = ConversationChain(
+        prompt=prompt,
+        llm=llm,
+        verbose=True,
+        memory=ConversationBufferMemory(human_prefix="Human"),
+            )
 
-    return llm_chain
+    return conversation
 
 
-llm_chain = create_llm_instance()
+conversation = create_llm_instance()
 
-st.title("ChatGPT-Like Clone: GPT4All")
+st.title("GPT4All Chat")
 
 
 if "llm_model" not in st.session_state:
@@ -42,20 +48,17 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+if user_input := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
-        st.markdown(prompt)
+        st.markdown(user_input)
 
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        response = llm_chain.invoke(prompt)
-        full_response += response["response"]
-        message_placeholder.markdown(full_response + "▌")
+        for token in conversation.stream(user_input):
+            full_response += token["response"]
+            message_placeholder.markdown(full_response + "▌")
     st.session_state.messages.append(
         {"role": "assistant", "content": full_response}
     )  # noqa: E501
-
-
-# st.write(llm_chain.memory.buffer)
